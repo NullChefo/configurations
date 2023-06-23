6442d56959749a5bfd88ee4d532ec45a884d3baa
nullchefo <stefan.kehayov.96@gmail.com>
2023-06-24 01:02:27 +0300

    add

--- linux/ubuntu-20.04/tools.sh
+++ linux/ubuntu-20.04/tools.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Check     https://github.com/FrameworkComputer/linux-docs/blob/main/Ubuntu22.04LTS-Manual-Setup-13thGen.md
+
 sudo apt-get update \
 && sudo apt-get upgrade \
 && sudo apt install flatpak \
@@ -28,6 +30,9 @@ sudo apt-get update \
 && sudo snap install gradle --edge --classic \
 && sudo snap install brackets --classic \
 && sudo snap install openjdk \
+# kvm
+&& sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils \
+
 
 # flatpack install
 && flatpak install flathub -y org.gnome.Extensions \
@@ -74,5 +79,39 @@ sudo apt-get update \
 && nvm install lts/* \
 && source ~/.bashrc \
 && curl -fsSL https://get.pnpm.io/install.sh | sh - \
-&& source ~/.bashrc 
+&& source ~/.bashrc \
 
+# Docker
+&& sudo apt-get update \
+&& sudo apt-get install ca-certificates curl gnupg \
+&& sudo install -m 0755 -d /etc/apt/keyrings \
+&& curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+&& sudo chmod a+r /etc/apt/keyrings/docker.gpg \
+&& echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
+&& sudo apt-get update \
+&& sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+# kvm
+&& modprobe kvm \
+&& modprobe kvm_intel \
+&& ls -al /dev/kvm \
+&& sudo usermod -aG kvm $USER \
+&& sudo usermod -a -G docker $USER \
+&& reboot
+# log out 
+
+# https://docs.docker.com/desktop/install/ubuntu/
+
+
+&& cd ~/Download \
+&& wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.20.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64 \
+
+
+
+# distrobox
+
+&& curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh \
+&& distrobox-create --name fedora-test --image fedora-38 \
+&& distrobox enter fedora-test \
