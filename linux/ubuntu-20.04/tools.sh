cd1c02ae911ba738756a6d53a9c6a624cec7af25
Stefan Kehayov <72579032+NullChefo@users.noreply.github.com>
2023-07-28 13:17:31 +0300

    Update tools.sh

--- linux/ubuntu-20.04/tools.sh
+++ linux/ubuntu-20.04/tools.sh
@@ -139,6 +139,7 @@ sudo apt-get update \
 
 
 && brew install gh \
+&& sudo apt -y install unzip zip \
 && curl -s "https://get.sdkman.io" | bash \
 # https://www.graalvm.org/downloads/
 && sdk install java 20.0.1-graal \
