57a33256f4f395e0da9bd60ba9f09c115e101a97
Stefan Kehayov <72579032+NullChefo@users.noreply.github.com>
2025-01-16 13:27:57 +0200

    Create install_choco_packages.ps1

---
+++ windows/install_choco_packages.ps1
@@ -0,0 +1,85 @@
+# RUN POWER_SHELL AS ADMIN
+
+choco install 7zip.install -y
+choco install brave -y
+choco install dotnet-sdk -y
+choco install parsec -y
+choco install bitwarden -y
+choco install podman-cli -y
+choco install podman-desktop -y
+choco install visualstudiocode -y
+choco install wsl2
+
+
+choco install microsoft-teams.install -y
+choco install postman -y
+choco install dbeaver -y
+choco install git.install -y
+choco install gimp -y
+choco install yt-dlg.install -y
+choco install opera -y
+choco install notion -y
+choco install joplin -y
+choco install paint.net -y
+choco install slack -y
+choco install wireshark -y
+choco install vlc.install -y
+choco install filezilla -y
+choco install awscli -y
+choco install libreoffice -y
+choco install putty.install -y
+choco install gimp -y
+choco install spotify -y
+choco install nodejs-lts -y
+choco install powertoys -y
+choco install itunes -y
+choco install cmake.install -y
+choco install openssl -y
+choco install audacity -y
+choco install terraform -y
+choco install ffmpeg -y
+choco install crystaldiskinfo -y
+choco install kubernetes-cli -y
+choco install gradle -y
+choco install golang -y
+choco install pnpm -y
+choco install yarn -y
+choco install powerbi -y
+choco install obsidian -y
+choco install parsec -y
+choco install discord.install -y
+choco install notepadplusplus.install -y
+choco install mongodb-compass -y
+choco install azure-cli -y
+choco install docker-desktop -y
+choco install signal -y
+choco install mingw -y
+choco install microsoftazurestorageexplorer -y
+choco install maven -y
+choco install qbittorrent -y
+choco install openvpn -y
+choco install flutter -y
+choco install obs-studio.install -y
+choco install iperf3 -y
+choco install cmder -y
+choco install discord.install -y
+choco install sublimetext3 -y
+choco install drawio -y
+choco install steam -y
+choco install epicgameslauncher -y
+choco install aimp -y
+choco install microsoft-teams.install -y
+
+choco install fiddler -y
+choco install gh -y
+choco install oh-my-posh -y
+choco install neovim -y
+choco install jetbrainstoolbox -y
+choco install vivaldi -y
+choco install cyberduck -y
+choco install rustup.install -y
+
+
+# https://www.notion.com/product/calendar#downloads
+
+# run rustup
